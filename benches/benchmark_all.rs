4e0b27c7ccde00b197673ea1f78408801002d32a
Jeroen Donkers <jeroen.donkers@live.nl>
2023-12-05 23:36:15 +0100

    Replace individual benchmark reports with violin plot

--- benches/benchmark_all.rs
+++ benches/benchmark_all.rs
@@ -5,7 +5,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("advent-of-code-2023");
     for day in days() {
         group.bench_with_input(
-            BenchmarkId::new(day.0, "benchmark"),
+            BenchmarkId::new(day.0, "original-puzzle-input"),
             &day.1,
             |b, &input| b.iter(|| day.2(&input)));
     }
