7f865dd92b788ebd319fc4f4c2f9ae94e9932adf
Jeroen Donkers <jeroen.donkers@live.nl>
2023-12-07 22:51:37 +0100

    Optimize puzzle day04a

--- src/days/day04a.rs
+++ src/days/day04a.rs
@@ -4,17 +4,27 @@ pub struct Day04a;
 
 impl Day for Day04a {
     fn run(input: &str) -> String {
-        input.lines()
-            .map(|line| line.split_once("|").unwrap())
-            .map(|(left, right)| (left.split_once(":").unwrap().1, right))
-            .map(|(winning, owned)| (winning.split_whitespace(), owned.split_whitespace()))
-            .map(|(winning, owned)| {
-                let owned = owned.filter(|owned_card| winning.clone()
-                    .any(|winning_card| *owned_card == winning_card))
-                    .collect::<Vec<&str>>();
-                if owned.is_empty() { 0 } else { 2usize.pow(owned.len() as u32 - 1) }
+        let colon_separator_index = input.find(|char| char == ':').unwrap();
+        let pipe_separator_index = input.find(|char| char == '|').unwrap();
+
+        input.as_bytes()
+            .split(|&b| b == b'\n')
+            .map(|line| {
+                let winning = &line[colon_separator_index+1..pipe_separator_index];
+                let owned = &line[pipe_separator_index+1..];
+
+                let won_amount = owned
+                    .chunks_exact(3)
+                    .map(|owned_card| &owned_card[1..])
+                    .filter(|owned_card| winning
+                        .chunks_exact(3)
+                        .map(|winning_card| &winning_card[1..])
+                        .any(|winning_card| &winning_card == owned_card))
+                    .count() as u32;
+
+                if won_amount == 0 { 0 } else { 2u32.pow(won_amount - 1) }
             })
-            .sum::<usize>()
+            .sum::<u32>()
             .to_string()
     }
 }
