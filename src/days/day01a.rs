86f98d75b3e4c217379cf89d231a726dcc89b861
Jeroen Donkers <jeroen.donkers@live.nl>
2023-12-10 17:53:22 +0100

    Optimize puzzle day01a

--- src/days/day01a.rs
+++ src/days/day01a.rs
@@ -5,11 +5,13 @@ pub struct Day01a;
 impl Day for Day01a {
     fn run(input: &str) -> String {
         input.lines()
-            .map(|line| line.chars().filter(|char| char.is_ascii_digit()))
-            .map(|mut digits| (digits.next(), digits.last()))
-            .map(|(a, b)| { (a.unwrap(), if b.is_some() { b.unwrap() } else { a.unwrap() }) })
-            .map(|(a, b)| format!("{}{}", a, b).parse::<usize>().unwrap())
-            .sum::<usize>()
+            .map(|line| (
+                line.chars().find(|char| char.is_ascii_digit()).unwrap(),
+                line.chars().rev().find(|char| char.is_ascii_digit()).unwrap()
+            ))
+            .map(|(first, last)|
+                first.to_digit(10).unwrap() * 10 + last.to_digit(10).unwrap())
+            .sum::<u32>()
             .to_string()
     }
 }
