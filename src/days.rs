58a08127125e497089b1b66349b0a181ce4c1f77
Jeroen Donkers <jeroen.donkers@live.nl>
2023-12-11 20:50:39 +0100

    Add puzzle day06a

--- src/days.rs
+++ src/days.rs
@@ -3,7 +3,7 @@ pub mod day02a; pub mod day02b;
 pub mod day03a; pub mod day03b;
 pub mod day04a; pub mod day04b;
 pub mod day05a; // pub mod day05b;
-// pub mod day06a; pub mod day06b;
+pub mod day06a; // pub mod day06b;
 // pub mod day07a; pub mod day07b;
 // pub mod day08a; pub mod day08b;
 // pub mod day09a; pub mod day09b;
